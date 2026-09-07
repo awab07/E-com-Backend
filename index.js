c92a0ca979eaa3f97780e28df27a8970241c9edd
M.Abdullah <abdullah488838@gmail.com>
2026-09-07 20:21:00 +0500

    fix-cors-issue

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://double-apple.vercel.app",
+        "https://triple-buzz.vercel.app",
         process.env.FRONTEND_URL
     ].filter(Boolean),
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
